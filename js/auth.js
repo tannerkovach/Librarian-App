19f036cf13f5f6179a95268138beed4d061b7aeb
Tanner Kovach <kovach.tanner@gmail.com>
2020-10-31 19:51:42 -0700

    working on filter logic

--- js/auth.js
+++ js/auth.js
@@ -9,20 +9,26 @@ const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
 const signInMsg = document.querySelector('.signInLibraryMsg');
 const emptyLibraryMsg = document.querySelector('.emptyLibraryMsg');
 const loggedInUser = document.querySelector('.loggedInUser');
-const userName = document.querySelector('.userName');
 const controlCenter = document.querySelector('.controlCenter');
 
 auth.onAuthStateChanged(user => {
     if (user) {
         const libraryRef = database.collection(`users/${user.uid}/library`).orderBy('timestamp', 'asc');
         
+        //if user is signed && user clicks filter unread, show unread books
         libraryRef
         .onSnapshot(function(data) {
             render(data.docs);
         })
-        // setTimeout(function(){ 
-        //     userName.innerHTML = `${user.displayName}'s Library`; 
-        // }, 500);
+        //if user is signed && user clicks filter read, show read books
+        const filteredLibraryRef = database.collection(`users/${user.uid}/library`).where("readStatus", "==", true);
+       
+        filteredLibraryRef
+        .onSnapshot(function(data) {
+            console.log(data.docs)
+        })
+
+        //else, render all books
     
         loggedInUser.innerHTML = `${user.displayName}`;
         loggedInUser.classList.remove('d-none');
@@ -52,7 +58,6 @@ auth.onAuthStateChanged(user => {
         consoleLog(`User is signed in: ${user.uid}`, 'success')
     }
     else {
-        // userName.innerHTML = '';
         loggedInUser.innerHTML = '';
         loggedInUser.classList.add('d-none');
         signup.classList.remove('d-none');
@@ -87,11 +92,13 @@ signupForm.addEventListener('submit', function(e) {
             displayName: displayName
         })
         .then(function() {
+            
+                loggedInUser.innerHTML = `${cred.user.displayName}`;
+                loggedInUser.classList.remove('d-none');
+            
             signupModal.hide();
             signupForm.reset();
-            setTimeout(function(){ 
-                userName.innerHTML = `${cred.user.displayName}'s Library`; 
-            }, 500);
+            
         })
         .catch(function(error) {
             console.log('error',error)
@@ -119,121 +126,3 @@ logout.addEventListener('click', function(e) {
     auth.signOut();
     render();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const signupForm = document.querySelector('#signupForm');
-// const signupModal = new bootstrap.Modal(document.getElementById('signupModal'));
-// const logout = document.querySelector('#logoutBtn');
-// const login = document.querySelector('#loginBtn');
-// const loginForm = document.querySelector('#loginForm');
-// const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-
-
-
-// auth.onAuthStateChanged((user) => {
-//     if (user) {
-//         console.log('user logged in: ', user)
-        
-//         // //render the posts associated to the current user
-//         const libraryRef = database.collection(`users/${user.uid}/library`)
-//         .orderBy('timestamp', 'asc')
-
-//         libraryRef
-//         .onSnapshot(function(snap) {
-//             render(snap.docs);
-//             // push()
-//         })
-//     }
-//     else {
-//         render();
-//     }
-// })
-
-// // sign up functionality
-// signupForm.addEventListener('submit', function(e) {
-//     e.preventDefault();
-
-//     let email = signupForm["signupEmail"].value, 
-//         password = signupForm["signupPassword"].value;
-        
-//     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        
-//         database.collection('users').doc(cred.user.uid).set({
-//             email: cred.user.email
-//         })
-//         .then(function() {
-//             signupModal.hide();
-//             signupForm.reset();
-           
-//         })
-//         .catch(function(error) {
-//             console.log('error',error)
-//         });
-//     })
-// });
-
-// // log in functionality
-
-   
-// loginForm.addEventListener('submit', function(e) {
-//     e.preventDefault();
-
-//     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-//     .then(function() {
-//         let email = loginForm["loginEmail"].value, 
-//             password = loginForm["loginPassword"].value;
-        
-//         auth.signInWithEmailAndPassword(email, password).then(cred => {
-//             console.log("user id:",cred.user.uid)
-
-//             loginModal.hide();
-//             loginForm.reset();
-       
-//         })
-//     })
-//     .catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//     });
-    
-// })
-  
-  
-
-
-
-// // sign out functionality
-// logout.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     firebase.auth().signOut()
-//     .then(() => {
-//         localStorage.clear()
-//         console.log('Sign Out')
-//     })
-//     .catch(e=>{
-//         console.error('Sign Out Error', e);
-//     });
-// })
-
